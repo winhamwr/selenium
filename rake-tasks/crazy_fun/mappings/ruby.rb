winhamwr/selenium

commit a518bab68e0280cc9ecc88168dd5433bff0d05c0

JariBakken: Update jruby-complete.jar to 1.5.3 (with Antwrap bundled).
Also removed the version number from the jar name and added a VERSION file.

git-svn-id: http://selenium.googlecode.com/svn/trunk@9916 07704840-8298-11de-bf8c-fd130f914ac9

rake-tasks/crazy_fun/mappings/ruby.rb
@@ -298,7 +298,7 @@ end # RubyMappings
 
 class RubyRunner
 
-  JRUBY_JAR = "third_party/jruby/jruby-complete-1.5.0.RC2.jar"
+  JRUBY_JAR = "third_party/jruby/jruby-complete.jar"
 
   def self.run(impl, opts)
     cmd = ["ruby"]

go
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar third_party/jruby/jruby-complete-1.5.0.RC2.jar -S rake $*
+java -jar third_party/jruby/jruby-complete.jar -S rake $*

go.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar third_party\jruby\jruby-complete-1.5.0.RC2.jar -S rake %*
+java -jar third_party\jruby\jruby-complete.jar -S rake %*

third_party/jruby/VERSION
@@ -1,0 +1,1 @@
+JRuby: 1.5.3 w/Antwrap bundled

commit 1d9228ebf81fc583889e391f662f88073be4f0b4

JariBakken: Work around jruby-complete/Windows bug, introduced in r9916.
git-svn-id: http://selenium.googlecode.com/svn/trunk@10245 07704840-8298-11de-bf8c-fd130f914ac9

rake-tasks/crazy_fun/mappings/ruby.rb
@@ -291,12 +291,13 @@ class RubyRunner
   JRUBY_JAR = "third_party/jruby/jruby-complete.jar"
 
   def self.run(impl, opts)
-    cmd = ["ruby"]
-
     if impl.to_sym == :jruby
       JRuby.runtime.instance_config.run_ruby_in_process = true
+
+      cmd = ["ruby"]
       cmd << "-J-Djava.awt.headless=true" if opts[:headless]
     else
+      cmd = [find_ruby]
       JRuby.runtime.instance_config.run_ruby_in_process = false
     end
 
@@ -320,6 +321,19 @@ class RubyRunner
     puts cmd.join(' ')
 
     sh(*cmd)
+  end
+
+  def self.find_ruby
+    return "ruby" unless windows?
+
+    # work around windows/jruby bug by searching the PATH ourselves
+    paths = ENV['PATH'].split(File::PATH_SEPARATOR)
+    paths.each do |path|
+      exe = File.join(path, "ruby.exe")
+      return exe if File.executable?(exe)
+    end
+
+    raise "could not find ruby.exe"
   end
 end
 