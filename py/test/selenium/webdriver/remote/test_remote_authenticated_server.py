winhamwr/selenium

commit 0459dc6ef96b90ba8fa8c51f81be2475b8d0e1e3

JasonLeyba on behalf of KenKania: Speed up python remote bindings by resolving hostname on init instead of once per command.
Fixes issue 1388.



git-svn-id: http://selenium.googlecode.com/svn/trunk@11673 07704840-8298-11de-bf8c-fd130f914ac9

py/selenium/webdriver/remote/remote_connection.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import logging
+import socket
 import string
 import urllib2
 import urlparse
@@ -114,6 +115,20 @@ class RemoteConnection(object):
     """
 
     def __init__(self, remote_server_addr):
+        # Attempt to resolve the hostname and get an IP address.
+        parsed_url = urlparse.urlparse(remote_server_addr)
+        if parsed_url.hostname:
+            try:
+                netloc = socket.gethostbyname(parsed_url.hostname)
+                if parsed_url.port:
+                    netloc += ':%d' % parsed_url.port
+                remote_server_addr = urlparse.urlunparse(
+                    (parsed_url.scheme, netloc, parsed_url.path,
+                     parsed_url.params, parsed_url.query, parsed_url.fragment))
+            except socket.gaierror:
+                LOGGER.info('Could not get IP address for host: %s' %
+                            parsed_url.hostname)
+
         self._url = remote_server_addr
         self._commands = {
             Command.NEW_SESSION: ('POST', '/session'),

py/selenium/webdriver/remote/webdriver.py
@@ -40,7 +40,7 @@ class WebDriver(object):
           this instance's session.
     """
 
-    def __init__(self, command_executor='http://localhost:4444/wd/hub',
+    def __init__(self, command_executor='http://127.0.0.1:4444/wd/hub',
                  desired_capabilities = None,
                  browser_profile=None):
         """Create a new driver that will issue commands using the wire protocol.
@@ -375,4 +375,3 @@ class WebDriver(object):
         """Gets the screenshot of the current window as a base64 encoded string which 
         is useful in embedded images in HTML."""
         return self.execute(Command.SCREENSHOT)['value']
-

commit 18634566e24f60a3d731b519f88aea0c2af22450

JasonLeyba: Fixing http basic auth for the python remote client, which was broken by r11673. Added a regression test so this sort of thing doesn't happen again.
git-svn-id: http://selenium.googlecode.com/svn/trunk@11719 07704840-8298-11de-bf8c-fd130f914ac9

Rakefile
@@ -403,7 +403,8 @@ task :test_firefox_py => [:webdriver_py, :firefox, "//javascript/firefox-driver:
   end
 end
 
-task :test_remote_py => [:webdriver_py, :remote_client, :'selenium-server-standalone'] do
+task :test_remote_py => [:webdriver_py, :remote_client, :'selenium-server-standalone',
+                         '//java/server/test/org/openqa/selenium/remote/server/auth:server:uber'] do
   if python? then
     py_setup = "build/python/bin/python " + 'setup.py build'
     py_test_path = 'build/python/bin/py.test'

py/selenium/webdriver/remote/remote_connection.py
@@ -122,6 +122,11 @@ class RemoteConnection(object):
                 netloc = socket.gethostbyname(parsed_url.hostname)
                 if parsed_url.port:
                     netloc += ':%d' % parsed_url.port
+                if parsed_url.username:
+                    auth = parsed_url.username
+                    if parsed_url.password:
+                      auth += ':%s' % parsed_url.password
+                    netloc = '%s@%s' % (auth, netloc)
                 remote_server_addr = urlparse.urlunparse(
                     (parsed_url.scheme, netloc, parsed_url.path,
                      parsed_url.params, parsed_url.query, parsed_url.fragment))

java/server/test/org/openqa/selenium/remote/server/auth/AuthenticatedWebDriverServer.java
@@ -1,0 +1,57 @@
+package org.openqa.selenium.remote.server.auth;
+
+import org.openqa.selenium.remote.server.DefaultDriverSessions;
+import org.openqa.selenium.remote.server.DriverServlet;
+
+import org.eclipse.jetty.http.security.Constraint;
+import org.eclipse.jetty.http.security.Password;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class AuthenticatedWebDriverServer {
+
+  public static void main(String[] args) throws Exception {
+    Server server = new Server();
+
+    Connector connector = new SelectChannelConnector();
+    connector.setPort(4444);
+    server.addConnector(connector);
+
+    Constraint constraint = new Constraint();
+    constraint.setName(Constraint.__BASIC_AUTH);
+    constraint.setRoles(new String[] { "user" });
+    constraint.setAuthenticate(true);
+
+    ConstraintMapping constraintMapping = new ConstraintMapping();
+    constraintMapping.setConstraint(constraint);
+    constraintMapping.setPathSpec("/*");
+
+    ConstraintSecurityHandler securityHandler = new ConstraintSecurityHandler();
+    securityHandler.addConstraintMapping(constraintMapping);
+
+    HashLoginService loginService = new HashLoginService();
+    loginService.putUser("fluffy", new Password("bunny"), new String[] {
+        "user"
+    });
+    securityHandler.setLoginService(loginService);
+    securityHandler.setAuthenticator(new BasicAuthenticator());
+
+    ServletContextHandler context = new ServletContextHandler(
+        ServletContextHandler.SESSIONS);
+    context.setContextPath("/wd/hub");
+    context.setAttribute(DriverServlet.SESSIONS_KEY,
+        new DefaultDriverSessions());
+    context.addServlet(new ServletHolder(DriverServlet.class), "/*");
+    context.setSecurityHandler(securityHandler);
+
+    server.setHandler(context);
+    server.start();
+  }
+}

java/server/test/org/openqa/selenium/remote/server/auth/build.desc
@@ -1,0 +1,9 @@
+java_library(name = "server",
+  main = "org.openqa.selenium.remote.server.auth.AuthenticatedWebDriverServer",
+  srcs = [ "*.java" ],
+  deps = [
+    "//java/client/src/org/openqa/selenium/firefox",
+    "//java/server/src/org/openqa/selenium/remote/server",
+    "//third_party/java/jetty",
+    "//third_party/java/servlet-api",
+  ])

py/test/selenium/webdriver/remote/test_remote_authenticated_server.py
@@ -1,0 +1,90 @@
+#!/usr/bin/env python
+# Copyright 2011 WebDriver committers
+# Copyright 2011 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import socket
+import subprocess
+import time
+import unittest
+import urllib
+import signal
+from selenium.test.selenium.webdriver.common.webserver import SimpleWebServer
+from selenium.test.selenium.webdriver.common import api_examples
+from selenium import webdriver
+
+SERVER_ADDR = "localhost"
+DEFAULT_PORT = 4444
+
+
+def wait_for_server(url, timeout):
+  start = time.time()
+  while time.time() - start < timeout:
+    try:
+      urllib.urlopen(url)
+      return 1
+    except IOError:
+      time.sleep(0.2)
+  return 0
+
+
+def setup_module(module):
+  _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  url = "http://%s:%d/wd/hub" % (SERVER_ADDR, DEFAULT_PORT)
+  try:
+    _socket.connect((SERVER_ADDR, DEFAULT_PORT))
+    print ("The remote driver server is already running or something else"
+           "is using port %d, continuing..." % DEFAULT_PORT)
+  except:
+    print ("Starting the remote driver server")
+    AuthenticatedServerTest.server_proc = subprocess.Popen(
+        "java -jar build/java/server/test/org/openqa/selenium/remote/server/auth/server-standalone.jar",
+        shell=True)
+    assert wait_for_server(url, 10), "can't connect"
+    print "Server should be online"
+  
+  webserver = SimpleWebServer()
+  webserver.start()
+  AuthenticatedServerTest.webserver = webserver
+
+
+def teardown_module(module):
+  try:
+    AuthenticatedServerTest.webserver.stop()
+  except AttributeError:
+      pass
+  # FIXME: This does not seem to work, the server process lingers
+  try:
+    os.kill(AuthenticatedServerTest.server_proc.pid, signal.SIGTERM)
+    time.sleep(5)
+  except:
+    pass
+
+
+class AuthenticatedServerTest(unittest.TestCase):
+  def setUp(self):
+    self.driver = None
+  
+  def tearDown(self):
+    if self.driver is not None:
+      self.driver.quit()
+  
+  def testShouldBeAbleToCreateAndUseASessionOnAnAuthenticatedServer(self):
+    self.driver = webdriver.Remote(
+        command_executor='http://fluffy:bunny@localhost:4444/wd/hub',
+        desired_capabilities=webdriver.DesiredCapabilities.FIREFOX)
+    self.driver.get('http://localhost:%d/simpleTest.html' %
+                    AuthenticatedServerTest.webserver.port)
+    self.assertEquals('Hello WebDriver', self.driver.title)