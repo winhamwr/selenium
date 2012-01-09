winhamwr/selenium

commit ebdce9f4040c89153a815f57c7aff36c0ebe62af

DanielWagnerHall: toolkit.telemetry.prompted is a boolean setting - set it thus.  Fixes issue 3144.
git-svn-id: http://selenium.googlecode.com/svn/trunk@15368 07704840-8298-11de-bf8c-fd130f914ac9

java/client/src/org/openqa/selenium/firefox/FirefoxProfile.java
@@ -93,7 +93,7 @@ public class FirefoxProfile {
           .put("security.warn_viewing_mixed.show_once", false)
           .put("signon.rememberSignons", false)
           .put("toolkit.networkmanager.disable", true)
-          .put("toolkit.telemetry.prompted", 2)
+          .put("toolkit.telemetry.prompted", true)
           .build();
 
   /**

commit 3042959810d0cfa9189e297014ca6c7b7fcb7de0

EranMes: Fixing Firefox 9 breakage introduced in r15368.
git-svn-id: http://selenium.googlecode.com/svn/trunk@15444 07704840-8298-11de-bf8c-fd130f914ac9

java/client/src/org/openqa/selenium/firefox/FirefoxProfile.java
@@ -93,7 +93,9 @@ public class FirefoxProfile {
           .put("security.warn_viewing_mixed.show_once", false)
           .put("signon.rememberSignons", false)
           .put("toolkit.networkmanager.disable", true)
-          .put("toolkit.telemetry.prompted", true)
+          .put("toolkit.telemetry.prompted", 2)
+          .put("toolkit.telemetry.enabled", false)
+          .put("toolkit.telemetry.rejected", true)
           .build();
 
   /**